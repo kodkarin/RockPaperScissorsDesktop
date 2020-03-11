19de079bd793bba6a7ff6b70fa347aaf0092294e
Karin <55209058+kodkarin@users.noreply.github.com>
2020-03-11 09:26:34 +0100

    game.fxml changed

--- src/main/java/com/example/rps/controllers/GameWindow.java
+++ src/main/java/com/example/rps/controllers/GameWindow.java
@@ -1,4 +1,32 @@
 package com.example.rps.controllers;
 
+import com.example.rps.models.Game;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+
+import java.awt.event.ActionEvent;
+
 public class GameWindow {
+
+    @FXML
+    private Label player1;
+    @FXML
+    private Label player2;
+    private Game game;
+    private int rounds = 0;
+
+    public GameWindow() {
+
+    }
+
+    @FXML
+    public void handleRockButtonAction() {
+        Label label = new Label("Rock");
+
+
+
+
+    }
 }
