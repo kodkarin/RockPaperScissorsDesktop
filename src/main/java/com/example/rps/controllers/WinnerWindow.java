8b34289ce33043c496ef0b7a78148f21a10d95a3
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-17 08:12:44 +0100

    Create textfields

--- src/main/java/com/example/rps/controllers/WinnerWindow.java
+++ src/main/java/com/example/rps/controllers/WinnerWindow.java
@@ -1,4 +1,18 @@
 package com.example.rps.controllers;
 
-public class WinnerWindow {
+import java.awt.*;
+
+public class WinnerWindow extends Window {
+
+    TextField textFieldActiveGameResult = new TextField();
+    TextField numberOfWinningsActivePlayer = new TextField();
+    TextField numberOfLossesActivePlayer = new TextField();
+
+    public void playAgainButtonClicked() {
+        // Skriv metod som startar nytt spel mot samma motspelare
+    }
+
+    public void activeGamesButtonClicked() {
+        getScreenController().setWindow(ScreenController.ACTIVE_GAMES, "");
+    }
 }
