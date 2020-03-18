f0656dbb6d1abfdf2f82856b25466dcf0939f5a5
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-18 21:00:20 +0100

    Create method to read helptext from file

--- src/main/java/com/example/rps/controllers/RulesForPlayGameWindow.java
+++ src/main/java/com/example/rps/controllers/RulesForPlayGameWindow.java
@@ -1,4 +1,49 @@
 package com.example.rps.controllers;
 
-public class RulesForPlayGameWindow {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class RulesForPlayGameWindow extends Window {
+
+    /*@FXML
+    Label text;
+
+    @FXML
+    public void initialize() {
+        try {
+            text.setText(
+                new String(
+                    ScreenController.getResAsStream("/text/helpText").readAllBytes(),
+                    StandardCharsets.UTF_8).strip());
+            System.out.println(text.isResizable());
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }*/
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    public void initialize() {
+
+        try {
+            File file = new File("src/main/resources/text/helpText");
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("Z");
+            String text = scanner.next();
+            textField.setText(text);
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Kan ej hitta fil");
+        }
+    }
 }
