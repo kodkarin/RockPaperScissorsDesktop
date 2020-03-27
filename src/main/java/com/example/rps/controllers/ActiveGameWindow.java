abf84911c8ec083703fa91e79ffb6783057a3601
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-27 12:25:57 +0100

    Updated helpButtonClicked to save active page and updated path that takes user to Rules

--- src/main/java/com/example/rps/controllers/ActiveGameWindow.java
+++ src/main/java/com/example/rps/controllers/ActiveGameWindow.java
@@ -11,11 +11,11 @@ public class ActiveGameWindow extends Window {
     }
 
     public void helpButtonClicked() {
-        super.previousPage = ScreenController.ACTIVE_GAMES;
+        getScreenController().setPreviousPage(ScreenController.ACTIVE_GAMES);
         getScreenController().setWindow(ScreenController.RULES, getToken());
     }
 
     public void logOutButtonClicked() {
-        getScreenController().setWindow(ScreenController.LOGIN, "");
+        getScreenController().setWindow(ScreenController.LOGIN, getToken());
     }
 }
