5fd82970e9223eb25d14975f54bf098c1d556d9c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-28 15:18:02 +0100

    Create imageView to accept GIF from FXML

--- src/main/java/com/example/rps/controllers/WinnerWindow.java
+++ src/main/java/com/example/rps/controllers/WinnerWindow.java
@@ -1,17 +1,32 @@
 package com.example.rps.controllers;
 
 import com.example.rps.models.Game;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 import java.awt.*;
+import java.sql.SQLException;
+
 
 public class WinnerWindow extends Window {
 
-    TextField textFieldActiveGameResult = new TextField();
-    TextField numberOfWinningsActivePlayer = new TextField();
-    TextField numberOfLossesActivePlayer = new TextField();
+    @FXML
+    private Label gameResultWinnerWindow;
+    @FXML
+    private Label numberOfWinningsActivePlayer;
+    @FXML
+    private Label numberOfLossesActivePlayer;
+    @FXML
+    private ImageView imageView;
+
+    Game game;
 
     public void playAgainButtonClicked() {
-        // Skriv metod som startar nytt spel mot samma motspelare
+        if (getUserId(getToken()) == game.getPlayer1().getUserId()) {
+            getScreenController().setWindow(ScreenController.GAME, getToken());
+        } else if (getUserId(getToken()) == game.getPlayer2().getUserId()) {
+            getScreenController().setWindow(ScreenController.GAME, getToken());
+        }
     }
 
     public void activeGamesButtonClicked() {
@@ -19,4 +34,16 @@ public class WinnerWindow extends Window {
     }
 
     public void initGame(Game game) {}
+
+    public void showResultFromPreviousGame() {
+        gameResultWinnerWindow.setText(game.getScorePlayer1() + " - " + game.getScorePlayer2());
+    }
+
+    public void showNumberOfWinningsForActivePlayer() throws SQLException {
+        if (getUserId(getToken()) == game.getPlayer1().getUserId()) {
+            numberOfWinningsActivePlayer = (Label) getConnection().prepareStatement("SELECT * FROM moves WHERE match_id = ? ORDER BY round_no");
+        } else if (getUserId(getToken()) == game.getPlayer2().getUserId()) {
+            getScreenController().setWindow(ScreenController.GAME, getToken());
+        }
+    }
 }
