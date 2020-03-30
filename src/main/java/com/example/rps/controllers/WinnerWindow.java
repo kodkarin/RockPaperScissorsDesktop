ace099e693ab0f9f1a1a7835e4e625a1c495e440
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-30 23:28:06 +0200

    Finished method to start new game, initGAme, showResultFromPreviousGame and to see gameresults. Create setUpWinnerWindow

--- src/main/java/com/example/rps/controllers/WinnerWindow.java
+++ src/main/java/com/example/rps/controllers/WinnerWindow.java
@@ -1,12 +1,10 @@
 package com.example.rps.controllers;
 
 import com.example.rps.models.Game;
-import com.example.rps.models.Player;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,59 +19,124 @@ public class WinnerWindow extends Window {
     @FXML
     private Label labelShowTotalNumberOfLossesForActivePlayer;
     @FXML
+    private Label labelHeaderWinnerWindow;
+    @FXML
     private ImageView imageView;
 
     private int numberOfWinningsActivePlayer;
     private int numberOfLossesActivePlayer;
     private int userIdPlayer1 = 0;
     private int userIdPlayer2 = 0;
+    private int userIdCpuPlayer = 0;
+    private String numberOfWinningsString = "";
+    private String numberOfLossesString = "";
 
     PreparedStatement getVictories = null;
     ResultSet results = null;
 
     Game game;
 
-    public WinnerWindow() {
+    public void initGame(Game game) {
+        this.game = game;
+        setUpWinnerWindow();
+    }
+
+    public void setUpWinnerWindow() {
         this.userIdPlayer1 = game.getPlayer1().getUserId();
         this.userIdPlayer2 = game.getPlayer2().getUserId();
+        this.userIdCpuPlayer = GameWindow.USER_ID_FOR_CPU_PLAYER;
+        this.numberOfWinningsString = String.valueOf(numberOfWinningsString);
+        this.numberOfLossesString = String.valueOf(numberOfLossesString);
+        this.showResultFromPreviousGame();
+        this.labelGameResultWinnerWindow.setText(game.getScorePlayer1() + " - " + game.getScorePlayer2());
+        this.labelShowTotalNumberOfWinningsForActivePlayer.setText(numberOfWinningsString);
+        this.labelShowTotalNumberOfLossesForActivePlayer.setText(numberOfLossesString);
+        this.labelHeaderWinnerWindow.setText("Congratulations, you won!");
+        this.labelHeaderWinnerWindow.setVisible(false);
+        this.labelShowTotalNumberOfWinningsForActivePlayer.setVisible(false);
+        this.labelShowTotalNumberOfLossesForActivePlayer.setVisible(false);
     }
 
     public void playAgainButtonClicked() {
-        PreparedStatement inviteFriendStatement = null;
 
-        try {
-            inviteFriendStatement = getConnection().prepareStatement("INSERT INTO invitations VALUES (?, ?);");
-            inviteFriendStatement.setInt(userIdPlayer1,getUserId(getToken()));
-            inviteFriendStatement.setInt(userIdPlayer2,getUserId(getToken()));
-            inviteFriendStatement.executeUpdate();
+        if (userIdPlayer1 == getUserId(getToken()) && userIdPlayer2 != GameWindow.USER_ID_FOR_CPU_PLAYER) {
+            PreparedStatement inviteFriendStatement = null;
 
-            getScreenController().setWindow(ScreenController.ACTIVE_GAMES, getToken());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
             try {
-                if(inviteFriendStatement != null) {
-                    inviteFriendStatement.close();
+                inviteFriendStatement = getConnection().prepareStatement("INSERT INTO invitations VALUES (?, ?);");
+                inviteFriendStatement.setInt(1,getUserId(getToken()));
+                inviteFriendStatement.setInt(2,getUserId(getToken()));
+                inviteFriendStatement.executeUpdate();
+
+                getScreenController().setWindow(ScreenController.ACTIVE_GAMES, getToken());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(inviteFriendStatement != null) {
+                        inviteFriendStatement.close();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
-            } catch (Exception ex) {
-                ex.printStackTrace();
+            }
+        } else if (userIdPlayer1 != getUserId(getToken())) {
+            PreparedStatement inviteFriendStatement = null;
+
+            try {
+                inviteFriendStatement = getConnection().prepareStatement("INSERT INTO invitations VALUES (?, ?);");
+                inviteFriendStatement.setInt(1,userIdPlayer1);
+                inviteFriendStatement.setInt(2,userIdPlayer2);
+                inviteFriendStatement.executeUpdate();
+
+                getScreenController().setWindow(ScreenController.ACTIVE_GAMES, getToken());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(inviteFriendStatement != null) {
+                        inviteFriendStatement.close();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        if (userIdPlayer2 == GameWindow.USER_ID_FOR_CPU_PLAYER) {
+            PreparedStatement startGameAgainstCpuStatement = null;
+
+            try {
+                startGameAgainstCpuStatement = getConnection().prepareStatement("INSERT INTO matches (player1, player2)" +
+                        "VALUES (?, ?);");
+                startGameAgainstCpuStatement.setInt(1, userIdPlayer1);
+                startGameAgainstCpuStatement.setInt(2, GameWindow.USER_ID_FOR_CPU_PLAYER);
+                startGameAgainstCpuStatement.executeUpdate();
+
+                getScreenController().setWindow(ScreenController.ACTIVE_GAMES, getToken());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(startGameAgainstCpuStatement != null) {
+                        startGameAgainstCpuStatement.close();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
-
 
     public void activeGamesButtonClicked() {
         getScreenController().setWindow(ScreenController.ACTIVE_GAMES, "");
     }
 
-    public void initGame(Game game) {}
-
     public void showResultFromPreviousGame() {
         labelGameResultWinnerWindow.setText(game.getScorePlayer1() + " - " + game.getScorePlayer2());
     }
 
-    @FXML
-    private int showTotalNumberOfWinningsForActivePlayer(ActionEvent event) throws SQLException {
+    private int showTotalNumberOfWinningsForActivePlayer() throws SQLException {
 
         if (userIdPlayer1 == game.getPlayer1().getUserId()) {
             getVictories = getConnection().prepareStatement("SELECT victories from friends WHERE player1 = ? AND player2 = ?");
@@ -83,23 +146,26 @@ public class WinnerWindow extends Window {
 
             if (results.next()) {
                 numberOfWinningsActivePlayer = results.getInt(userIdPlayer1);
+                labelShowTotalNumberOfWinningsForActivePlayer.setVisible(true);
+                labelHeaderWinnerWindow.setVisible(true);
             }
         } else if (userIdPlayer2 == game.getPlayer2().getUserId()) {
             getVictories = getConnection().prepareStatement("SELECT victories from friends WHERE player1 = ? AND player2 = ?");
-            getVictories.setInt(1,userIdPlayer2);
-            getVictories.setInt(2,userIdPlayer1);
+            getVictories.setInt(1,userIdPlayer1);
+            getVictories.setInt(2,userIdPlayer2);
             results = getVictories.executeQuery();
 
             if (results.next()) {
                 numberOfWinningsActivePlayer = results.getInt(userIdPlayer2);
+                labelShowTotalNumberOfWinningsForActivePlayer.setVisible(true);
+                labelHeaderWinnerWindow.setVisible(true);
             }
         }
 
         return numberOfWinningsActivePlayer;
     }
 
-    @FXML
-    private int showTotalNumberOfLossesForActivePlayer(ActionEvent event) throws SQLException {
+    private int showTotalNumberOfLossesForActivePlayer() throws SQLException {
 
         if (userIdPlayer1 == game.getPlayer1().getUserId()) {
             getVictories = getConnection().prepareStatement("SELECT victories from friends WHERE player1 = ? AND player2 = ?");
@@ -109,6 +175,7 @@ public class WinnerWindow extends Window {
 
             if (results.next()) {
                 numberOfLossesActivePlayer = results.getInt(userIdPlayer1);
+                labelShowTotalNumberOfLossesForActivePlayer.setVisible(true);
             }
         }
 
@@ -120,6 +187,7 @@ public class WinnerWindow extends Window {
 
             if (results.next()) {
                 numberOfLossesActivePlayer = results.getInt(userIdPlayer2);
+                labelShowTotalNumberOfLossesForActivePlayer.setVisible(true);
             }
         }
 
